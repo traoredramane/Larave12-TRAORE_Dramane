21b477de597b6622fddfd90a669169ed8567df9d
traoredramane <trdramane98@gmail.com>
2023-11-03 10:09:39 +0400

    fin

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-
+use Illuminate\Auth\AuthenticationException;
 class Handler extends ExceptionHandler
 {
     /**
@@ -48,4 +48,30 @@ class Handler extends ExceptionHandler
     {
         return parent::render($request, $exception);
     }
+    
+    
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        $guard = array_get($exception->guards(),0);
+        switch ($variable) {
+            case 'admin':
+               $redirect = redirect()->guest(route('admin.login'));
+                break;
+            
+            default:
+            $redirect = redirect()->guest(route('login'));
+                break;
+        }
+        return $request->expectsJson()
+                    ? response()->json(['message' => $exception->getMessage()], 401)
+                    :$redirect ?? route('login');
+    }
+
 }
--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,6 +7,17 @@ use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
+    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
     public function index()
     {
         return view('admin.home');
